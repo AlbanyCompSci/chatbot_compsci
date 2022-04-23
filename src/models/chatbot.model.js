cd642f16c44ffc17983606fc9dd22532a0d238d6
theMackabu <theMackabu@gmail.com>
2022-04-23 02:53:52 +0000

    more

--- src/models/chatbot.model.js
+++ src/models/chatbot.model.js
@@ -6,13 +6,23 @@ const Chatlog = mongoose.model(
 );
 
 global.getChatResponse = async function log(query, session, timestamp) {
+	let res_error = [
+		'sorry, could you please repeat that again?',
+		'hmm, did I hear something?',
+		'wait, what did you say?',
+		'excuse me, but I did not hear you correctly, could you please repeat that?',
+		'sorry, but I could not understand...',
+	];
+
+	let random_not_found = res_error[Math.floor(Math.random() * res_error.length)];
+
 	return chatbot.generateChatReply(query).then((res, err) => {
 		if (res.answers.length == 0) {
 			const chatlog = new Chatlog({
 				timestamp: timestamp,
 				session: session,
 				query: query,
-				response: 'sorry, could you please repeat that again?',
+				response: random_not_found,
 				raw: res,
 			});
 			chatlog.save((err) => {
@@ -20,7 +30,7 @@ global.getChatResponse = async function log(query, session, timestamp) {
 					console.log(err);
 				}
 			});
-			return { res, answer: 'sorry, could you please repeat that again?' };
+			return { res, answer: random_not_found };
 		} else {
 			const chatlog = new Chatlog({
 				timestamp: timestamp,
